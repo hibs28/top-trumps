07eec4ab7a0f70ec8f8730e0d17593a2191d3488
maishah8 <maishahussain19@gmail.com>
2025-01-13 18:45:33 +0000

    Update initial-task.py

--- initial-task.py
+++ initial-task.py
@@ -5,6 +5,8 @@ import random
 from pprint import pprint
 
 pokemon_number = random.randint(1, 151)
+#generate a random number between 1 and 151
+
 url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
 response = requests.get(url)
 pokemon = response.json()
